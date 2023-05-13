2bca6f8845b43afce472e7591bb3b0c5317740fe
Abhishek  kandari <abhishekkandari96@gmail.com>
2023-05-13 12:06:18 +0530

    HDFC bank completed

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -3,4 +3,11 @@ public interface BankAccount {
     //withdraw money feature
     boolean withdrawMoney(int money);
     //check bank balance
+    int checkBankBalance(int money);
+    //add money
+    boolean addMoney(int money);
+    //get roi
+    int getROI();
+    //get total interest
+    int getTotalInterest(int amount, int time);
 }
--- src/SBIBankAccount.java
+++ src/SBIBankAccount.java
@@ -4,4 +4,24 @@ public class SBIBankAccount implements BankAccount{
     public boolean withdrawMoney(int money) {
         return false;
     }
+
+    @Override
+    public int checkBankBalance(int money) {
+        return 0;
+    }
+
+    @Override
+    public boolean addMoney(int money) {
+        return false;
+    }
+
+    @Override
+    public int getROI() {
+        return 0;
+    }
+
+    @Override
+    public int getTotalInterest(int amount, int time) {
+        return 0;
+    }
 }
